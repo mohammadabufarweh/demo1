46206c8c168d7c314f7f8ef8075c907eacea5c37
Mohammadabufarweh <abufarweh34@gmail.com>
2021-05-02 05:01:33 +0300

    slider

--- Js/jsMain.js
+++ Js/jsMain.js
@@ -1,18 +1,54 @@
-'use stict'
-let slideIndex = 1;
-    showDivs(slideIndex);
+'use srict'
 
-    function plusDivs(n) {
-        showDivs(slideIndex += n);
-    }
+var slides = document.querySelectorAll('.slide');
+var btns = document.querySelectorAll('.btn');
+let currentSlide = 1;
 
-    function showDivs(n) {
-        let i;
-        let x = document.getElementsByClassName("mainSlideShow");
-        if (n > x.length) { slideIndex = 1 }
-        if (n < 1) { slideIndex = x.length };
-        for (i = 0; i < x.length; i++) {
-            x[i].style.display = "none";
-        }
-        x[slideIndex - 1].style.display = "block";
+// Javascript for image slider manual navigation
+var manualNav = function(manual){
+  slides.forEach((slide) => {
+    slide.classList.remove('active');
+
+    btns.forEach((btn) => {
+      btn.classList.remove('active');
+    });
+  });
+
+  slides[manual].classList.add('active');
+  btns[manual].classList.add('active');
+}
+
+btns.forEach((btn, i) => {
+  btn.addEventListener("click", () => {
+    manualNav(i);
+    currentSlide = i;
+  });
+});
+
+// Javascript for image slider autoplay navigation
+var repeat = function(activeClass){
+  let active = document.getElementsByClassName('active');
+  let i = 1;
+
+  var repeater = () => {
+    setTimeout(function(){
+      [...active].forEach((activeSlide) => {
+        activeSlide.classList.remove('active');
+      });
+
+    slides[i].classList.add('active');
+    btns[i].classList.add('active');
+    i++;
+
+    if(slides.length == i){
+      i = 0;
     }
+    if(i >= slides.length){
+      return;
+    }
+    repeater();
+  }, 10000);
+  }
+  repeater();
+}
+repeat();
